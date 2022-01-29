5412545c49cb5d211b4d53942b256f7a410b1716
Gabriel Lopes <gabriel.lopes_glp@outlook.com>
2022-01-29 18:20:43 -0300

    complementary challenge completed

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -45,6 +45,19 @@ defmodule GenReport do
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
+  def build_from_many(filenames) when not is_list(filenames) do
+    {:error, "Insira uma lista de arquivos"}
+  end
+
+  def build_from_many(filenames) do
+    result =
+      filenames
+      |> Task.async_stream(&build/1)
+      |> Enum.reduce(report_acc(), fn {:ok, result}, report -> sum_reports(report, result) end)
+
+    {:ok, result}
+  end
+
   defp sum_values([name, quantity_hours, _day, month, year], %{
          "all_hours" => all_hours,
          "hours_per_month" => hours_per_month,
@@ -59,6 +72,33 @@ defmodule GenReport do
       put_in(hours_per_year[name][year], hours_per_year[name][year] + quantity_hours)
 
     build_report(all_hours, hours_per_month, hours_per_year)
+  end
+
+  defp sum_reports(
+         %{
+           "all_hours" => all_hours1,
+           "hours_per_month" => hours_per_month1,
+           "hours_per_year" => hours_per_year1
+         },
+         %{
+           "all_hours" => all_hours2,
+           "hours_per_month" => hours_per_month2,
+           "hours_per_year" => hours_per_year2
+         }
+       ) do
+    all_hours = merge_maps(all_hours1, all_hours2)
+    hours_per_month = merge_nested_maps(hours_per_month1, hours_per_month2)
+    hours_per_year = merge_nested_maps(hours_per_year1, hours_per_year2)
+
+    build_report(all_hours, hours_per_month, hours_per_year)
+  end
+
+  defp merge_maps(map1, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 -> value1 + value2 end)
+  end
+
+  defp merge_nested_maps(map1, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 -> merge_maps(value1, value2) end)
   end
 
   defp report_acc do
--- test/gen_report_test.exs
+++ test/gen_report_test.exs
@@ -19,4 +19,22 @@ defmodule GenReportTest do
       assert response == {:error, "Insira o nome de um arquivo"}
     end
   end
+
+  describe "build_from_many/1" do
+    test "when a file list is provided, builds the report" do
+      filenames = ["part_1.csv", "part_2.csv", "part_3.csv"]
+
+      response = GenReport.build_from_many(filenames)
+
+      assert response == {:ok, ReportFixture.build()}
+    end
+
+    test "when a file list is not provided, returns an error" do
+      response = GenReport.build_from_many("capivara.txt")
+
+      expected_response = {:error, "Insira uma lista de arquivos"}
+
+      assert response == expected_response
+    end
+  end
 end
